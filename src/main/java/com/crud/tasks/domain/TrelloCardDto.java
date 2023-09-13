6c6a64a3bebacb79d7288bfbe2354d557768de00
wojmys <124149391+wojmys@users.noreply.github.com>
2023-09-13 16:46:08 +0200

    update (#1)

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -2,9 +2,11 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TrelloCardDto {
 
     private String name;
